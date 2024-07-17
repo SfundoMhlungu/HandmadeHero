cb88d236601b6222c17bd8134d76675119c6154c
SfundoMhlungu <devadress21@gmail.com>
2024-07-17 08:52:00 +0200

    changed from zig c++ to clang: zing might remove llvm support

--- src/build.bat
+++ src/build.bat
@@ -1,11 +1,8 @@
-@echo off
+if not exist ..\build mkdir ..\build
+pushd ..\build
 
-IF NOT EXIST ..\..\build mkdir ..\..\build
-pushd ..\..\build
-
-REM Compiling C++ file with MinGW
-REM -Wno-writable-strings -Wno-unused-parameter - - turn off warning 
-REM  implicit sign conversion is taking place, potentially resulting in data loss
- zig c++ -g -W -Wconversion -Wno-sign-conversion ..\handmade\src\win32_handmade.cpp -lgdi32 -lXInput1_4 -ldsound  -o main.exe
+REM Compiling C++ file with g++
+REM -Wno-writable-strings -Wno-unused-parameter - turn off warnings about writable strings and unused parameters
+clang++ -g -Wall -Wextra -Wno-writable-strings -Wno-unused-parameter ..\src\win32_handmade.cpp -lgdi32 -lXInput1_4 -ldsound -o main.exe
 
 popd
